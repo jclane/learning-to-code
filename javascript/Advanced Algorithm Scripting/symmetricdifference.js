4ca642b02c16a6ea3ea931534d3f86c5d23665ed
Justin Lane <j.lane@twc.com>
2018-01-10 16:46:53 +0000

    Finally working.

--- javascript/Advanced Algorithm Scripting/symmetricdifference.js
+++ javascript/Advanced Algorithm Scripting/symmetricdifference.js
@@ -13,12 +13,25 @@ both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
 
 function sym(args) {
   args = Array.prototype.slice.call(arguments);
-
+  
   let result = [];
   
-  // Reduces 'args' to an 1 deminsional array and counts how many times a 
-  // value appears in the new array
-  let reduce = args.reduce((a, b) => a.concat(b))
+  // Reduces 'args' to an 1 dimensional array and counts how many times a 
+  // value appears in the new array.
+  function symdiff(arr1, arr2) {
+    result = [];
+    
+    function filterArr(arr) {
+      let filter = arr.filter(function(item, pos) {
+        return arr.indexOf(item) == pos;
+      });
+      return filter;
+    }
+    
+    arr1 = filterArr(arr1);
+    arr2 = filterArr(arr2);
+    
+    let reduce = arr1.concat(arr2)
                 .reduce(function (allValues, value) {
                   if (value in allValues) {
                     allValues[value]++;
@@ -27,19 +40,30 @@ function sym(args) {
                   }
                   return allValues;
                 }, {});
-                
-  for (var key in reduce) { 
-    if (reduce[key] === 1) {
-      result.push(parseInt(key));
+  
+    // Push values that only appear in 'reduce' once to the 'result' array.              
+    for (var key in reduce) { 
+      if (reduce[key] === 1) {
+        result.push(parseInt(key));
+      }
     }
+    
+    args.splice(0,2);
+    args.push(result);
   }
-                
+  
+  while (args.length >= 2) {
+    symdiff(args[0],args[1]);
+  }
                 
   return result;
 }
 
-console.log(sym([1, 2, 3], [5, 2, 1, 4]));
-console.log("Should be [3, 4, 5]");
+//console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+//console.log("Should be [3, 4, 5]");
 
-console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]));
-console.log("Should be [1, 4, 5]");
+//console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]));
+//console.log("Should be [1, 4, 5]");
+
+console.log("Should return [1, 4, 5]");
+console.log(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]));
