bf568761b8cdb56c4a9b778e95eae59a87e32ad2
Justin Lane <jclane@users.noreply.github.com>
2018-04-15 14:04:19 -0400

    Update minCat.java

--- java/codingbat/string-1/minCat.java
+++ java/codingbat/string-1/minCat.java
@@ -2,7 +2,7 @@
 # However, if the strings are different lengths, omit chars from the longer string so it is 
 # the same length as the shorter string. So "Hello" and "Hi" yield "loHi". 
 # The strings may be any length.
-
+#
 # minCat("Hello", "Hi") → "loHi"
 # minCat("Hello", "java") → "ellojava"
 # minCat("java", "Hello") → "javaello"
