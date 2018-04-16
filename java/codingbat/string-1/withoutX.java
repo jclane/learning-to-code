5401e05c7799d5a370bd974c77a68227f7167ffa
Justin Lane <jclane@users.noreply.github.com>
2018-04-16 10:54:33 -0400

    Update withoutX.java

--- java/codingbat/string-1/withoutX.java
+++ java/codingbat/string-1/withoutX.java
@@ -1,9 +1,11 @@
-# Given a string, if the first or last chars are 'x', return the string without those 'x' chars, 
-# and otherwise return the string unchanged.
-#
-# withoutX("xHix") → "Hi"
-# withoutX("xHi") → "Hi"
-# withoutX("Hxix") → "Hxi"
+/*
+Given a string, if the first or last chars are 'x', return the string without those 'x' chars, 
+and otherwise return the string unchanged.
+
+withoutX("xHix") → "Hi"
+withoutX("xHi") → "Hi"
+withoutX("Hxix") → "Hxi"
+*/
 
 public String withoutX(String str) {
   if (str.length() < 2) {
