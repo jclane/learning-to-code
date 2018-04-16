0857820e97452399ec39fc88d6ac21e414d1b746
Justin Lane <jclane@users.noreply.github.com>
2018-04-16 10:53:10 -0400

    Update twoChar.java

--- java/codingbat/string-1/twoChar.java
+++ java/codingbat/string-1/twoChar.java
@@ -1,10 +1,12 @@
-# Given a string and an index, return a string length 2 starting at the given index. 
-# If the index is too big or too small to define a string length 2, use the first 2 chars. 
-# The string length will be at least 2.
-#
-# twoChar("java", 0) → "ja"
-# twoChar("java", 2) → "va"
-# twoChar("java", 3) → "ja"
+/*
+Given a string and an index, return a string length 2 starting at the given index. 
+If the index is too big or too small to define a string length 2, use the first 2 chars. 
+The string length will be at least 2.
+
+twoChar("java", 0) → "ja"
+twoChar("java", 2) → "va"
+twoChar("java", 3) → "ja"
+*/
 
 public String twoChar(String str, int index) {
   if (index > 0 && index <= str.length() - 2) {
