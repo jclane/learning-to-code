ecf38c599e88b7383c832d1621ea53960060004f
Justin Lane <jclane@users.noreply.github.com>
2018-04-16 09:58:05 -0400

    Update atFirst.java

--- java/codingbat/string-1/atFirst.java
+++ java/codingbat/string-1/atFirst.java
@@ -1,9 +1,11 @@
-# Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2,
-# use '@' for the missing chars.
-#
-# atFirst("hello") → "he"
-# atFirst("hi") → "hi"
-# atFirst("h") → "h@"
+/*
+Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2,
+use '@' for the missing chars.
+
+atFirst("hello") → "he"
+atFirst("hi") → "hi"
+atFirst("h") → "h@"
+*/
 
 public String atFirst(String str) {
   int length = str.length();
