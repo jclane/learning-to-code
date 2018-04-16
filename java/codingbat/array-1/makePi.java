9808036d2c250037a4192599c4c54180be0284b3
Justin Lane <jclane@users.noreply.github.com>
2018-04-16 09:52:56 -0400

    Update makePi.java

--- java/codingbat/array-1/makePi.java
+++ java/codingbat/array-1/makePi.java
@@ -1,6 +1,8 @@
-# Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
-#
-# makePi() → [3, 1, 4]
+/*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+makePi() → [3, 1, 4]
+*/
 
 public int[] makePi() {
   int[] arr = {3,1,4};
