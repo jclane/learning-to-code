f45f29a21d1b5a92b1f8fd9956b65f681901e611
Justin Lane <jclane@users.noreply.github.com>
2018-04-16 10:02:06 -0400

    Update firstHalf.java

--- java/codingbat/string-1/firstHalf.java
+++ java/codingbat/string-1/firstHalf.java
@@ -1,8 +1,10 @@
-# Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
-#
-# firstHalf("WooHoo") → "Woo"
-# firstHalf("HelloThere") → "Hello"
-# firstHalf("abcdef") → "abc"
+/*
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+firstHalf("WooHoo") → "Woo"
+firstHalf("HelloThere") → "Hello"
+firstHalf("abcdef") → "abc"
+*/
 
 public String firstHalf(String str) {
   if (str.length() % 2 == 0) {
