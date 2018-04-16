78c88ef2cb9a77ed2d3f665cfc8bb79979e7d291
Justin Lane <jclane@users.noreply.github.com>
2018-04-16 10:44:50 -0400

    Update seeColor.java

--- java/codingbat/string-1/seeColor.java
+++ java/codingbat/string-1/seeColor.java
@@ -1,8 +1,10 @@
-# Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string.
-#
-# seeColor("redxx") → "red"
-# seeColor("xxred") → ""
-# seeColor("blueTimes") → "blue"
+/*
+Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string.
+
+seeColor("redxx") → "red"
+seeColor("xxred") → ""
+seeColor("blueTimes") → "blue"
+*/
 
 public String seeColor(String str) {
   if (str.startsWith("red")) {
