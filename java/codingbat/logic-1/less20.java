388656ba90e19d50a02eb34f984804d545c3d8c1
Justin Lane <jclane@users.noreply.github.com>
2018-04-17 11:19:36 -0400

    Create less20.java

---
+++ java/codingbat/logic-1/less20.java
@@ -0,0 +1,16 @@
+/*
+Return true if the given non-negative number is 1 or 2 less than a multiple of 20. 
+So for example 38 and 39 return true, but 40 returns false.
+
+less20(18) → true
+less20(19) → true
+less20(20) → false
+*/
+
+public boolean less20(int n) {
+  if (20 % n == 1 || 20 % n == 2 || n % 20 == 18 || n % 20 == 19) {
+    return true;
+  } else {
+    return false;
+  }
+}
