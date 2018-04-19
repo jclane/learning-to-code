4d96e132782cccce00f6bb9019884af0d7c82b23
Justin Lane <jclane@users.noreply.github.com>
2018-04-19 16:40:45 -0400

    Update has22.java

--- java/codingbat/array-2/has22.java
+++ java/codingbat/array-2/has22.java
@@ -1,5 +1,5 @@
 /*
- *Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
+ * Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
  *
  * has22([1, 2, 2]) → true
  * has22([1, 2, 1, 2]) → false
